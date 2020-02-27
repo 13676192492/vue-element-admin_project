2f0bd0b0de1bb11fbc5d56a703f9d9bf16f27171
xiaojiawei <1015881163@qq.com>
2020-02-27 15:18:07 +0800

    支付接口

---
+++ src/views/property/pay.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="app-container">
+
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Pay',
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
