014d4dba56e3e4283c9c261c971d671dbbd629ad
Ying <569745692@qq.com>
2020-03-05 13:13:28 +0800

    添加金额单位

--- src/views/adminSpendingOrder/components/OrderDetails.vue
+++ src/views/adminSpendingOrder/components/OrderDetails.vue
@@ -10,7 +10,7 @@
         <li>客户账号：{{param.data.user.userName}}</li>
         <li>类型：{{param.data.orderType}}</li>
         <li>创建时间：{{param.data.createdOn}}</li>
-        <li>金额：{{param.data.orderTotal}}</li>
+        <li>金额：{{param.data.orderTotal}} 元</li>
         <li>支付状态：{{param.data.orderStatus}}</li>
         <li>支付时间：{{param.data.paymentOn}}</li>
       </ul>
