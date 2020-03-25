4c7242472237caab5dcd167a84a9daf8c7ec6e87
Ying <569745692@qq.com>
2020-03-25 11:07:14 +0800

    添加应用话单补录功能

--- src/api/applicationBill/index.js
+++ src/api/applicationBill/index.js
@@ -17,3 +17,20 @@ export function getAdminApplicationBillList(params) {
         data: params
     })
 }
+
+//获取所有应用数据
+export function getAllAppId() {
+    return request({
+        url: '/api/admin/application/all',
+        method: 'get',
+    })
+}
+
+// 补录
+export function setSupplementData(params) {
+    return request({
+        url: '/api/admin/apprecord/create',
+        method: 'post',
+        data: params
+    })
+}
--- src/views/adminApplicationBill/index.vue
+++ src/views/adminApplicationBill/index.vue
@@ -17,25 +17,42 @@
         style="margin-right:1%;"
       ></el-date-picker>
 
-      <el-button type="primary" icon="el-icon-search" @click="getData">查询</el-button>
+      <el-button type="primary" icon="el-icon-search" @click="getData"
+        >查询</el-button
+      >
+    </div>
+
+    <div class="toolbar-group">
+      <button
+        type="button"
+        class="el-button el-button--text el-button--mini"
+        @click="openAddCom()"
+      >
+        <span> <i class="el-icon-plus" /> 补录 </span>
+      </button>
     </div>
 
     <el-table
       v-loading="loading"
       :data="list"
-      :header-cell-style="{background:'#eef1f6',color:'#606266'}"
+      :header-cell-style="{ background: '#eef1f6', color: '#606266' }"
       border
       fit
       highlight-current-row
       style="width: 100%;"
     >
-      <el-table-column label="序号" width="60" type="index" :index="tableIndex" />
+      <el-table-column
+        label="序号"
+        width="60"
+        type="index"
+        :index="tableIndex"
+      />
       <el-table-column label="appid" prop="appId"></el-table-column>
       <el-table-column label="应用名称" prop="iotName"></el-table-column>
       <el-table-column label="统计日期">
         <template slot-scope="{ row }">
-            <span>{{row.totalTime|changeTime}}</span>
-          </template>
+          <span>{{ row.totalTime | changeTime }}</span>
+        </template>
       </el-table-column>
       <el-table-column label="费用（元）" prop="fee"></el-table-column>
     </el-table>
@@ -50,10 +67,61 @@
       @size-change="handleSizeChange"
       @current-change="handleCurrentChange"
     />
+
+    <el-dialog
+      title="补录应用话单"
+      :close-on-click-modal="false"
+      :visible.sync="dialogVisible"
+      :append-to-body="true"
+    >
+      <el-form
+        ref="form"
+        :inline="true"
+        :model="form"
+        :rules="rules"
+        size="medium"
+        label-width="100px"
+      >
+        <el-form-item label="AppId" prop="appId">
+          <el-select v-model="form.appId" filterable placeholder="请选择AppId">
+            <el-option
+              v-for="item in options"
+              :key="item.appId"
+              :label="item.iotName"
+              :value="item.appId"
+            >
+            </el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="统计时间" prop="totalDateTime">
+          <el-date-picker
+            v-model="form.totalDateTime"
+            type="date"
+            placeholder="请输入统计时间"
+          >
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item label="费用金额" prop="fee">
+          <el-input
+            v-model="form.fee"
+            type="number"
+            placeholder="请输入费用金额"
+          />
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="resetForm('form')">取 消</el-button>
+        <el-button type="primary" @click="addBill('form')">确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 <script>
-import { getAdminApplicationBillList } from "@/api/applicationBill/index";
+import {
+  getAdminApplicationBillList,
+  getAllAppId,
+  setSupplementData
+} from "@/api/applicationBill/index";
 import { updateTime, changeTimeFormat } from "@/assets/publicScript/public";
 export default {
   data() {
@@ -78,6 +146,29 @@ export default {
       param: {
         isShow: false
       },
+      dialogVisible: false,
+      form: {
+        appId:null,
+        totalDateTime:null,
+        fee:null
+      },
+      rules: {
+        appId: [
+          { required: true, message: "必须选择appId", trigger: "change" }
+        ],
+        totalDateTime: [
+          { required: true, message: "必须填写统计时间", trigger: "change" }
+        ],
+        fee: [
+          { required: true, message: "必须填写费用金额", trigger: "change" },
+          {
+            pattern: /((^[1-9]\d*)|^0)(\.\d{0,2}){0,1}$/,
+            message: "必须输入正确金额",
+            trigger: "blur"
+          }
+        ]
+      },
+      options: [],
       total: 1,
       selectTime: null,
       list: []
@@ -85,9 +176,9 @@ export default {
   },
   filters: {
     changeTime(val) {
-      let time = '-'
+      let time = "-";
       if (val) {
-        time = val.slice(0, 4) + "-" + val.slice(4, 6) + "-" + val.slice(6);       
+        time = val.slice(0, 4) + "-" + val.slice(4, 6) + "-" + val.slice(6);
       }
       return time;
     }
@@ -96,7 +187,7 @@ export default {
     this.getList();
   },
   methods: {
-    getData(){
+    getData() {
       this.params.page = 1;
       this.getList();
     },
@@ -146,6 +237,35 @@ export default {
       this.param.isShow = true;
       this.param.data = params;
       this.$refs.getDetails.getList(this.params.no);
+    },
+    openAddCom() {
+      this.dialogVisible = true;
+      getAllAppId().then(res => {
+        if (res.data.success) {
+          this.options = res.data.data;
+        }
+      });
+    },
+    addBill(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          this.form.fee = parseInt(this.form.fee);
+          setSupplementData(this.form).then(res => {
+            if (res.data.success) {
+              this.$notify({
+                title: "成功",
+                message: "添加成功",
+                type: "success"
+              });
+              this.resetForm(formName);
+            }
+          });
+        }
+      });
+    },
+    resetForm(formName) {
+      this.dialogVisible = false;
+      this.$refs[formName].resetFields();
     }
   }
 };
@@ -158,5 +278,9 @@ export default {
 }
 /deep/ .el-table__header {
   width: 100% !important;
+}
+
+/deep/[class^="el-icon-"] {
+  font-weight: bold;
 }
 </style>
